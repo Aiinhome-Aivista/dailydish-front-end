2c2f8f9baa155ba28c707f77bacd4b285aac5f86
PSpabitra <sarkarpabi42@gmail.com>
2026-01-17 15:57:16 +0530

    splashScreen

--- src/features/splashscreen/SplashScreen.tsx
+++ src/features/splashscreen/SplashScreen.tsx
@@ -1,20 +1,20 @@
-import BowlIcon from "../assets/Layer 3.svg";
+import BowlIcon from "../../assets/Layer 3.svg";
 
 const SplashScreen = () => {
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
+    <div className="min-h-screen bg-[#435334] flex items-center justify-center">
       {/* Outer rounded container */}
       <div className="w-[96%] h-[92vh] bg-olive rounded-2xl flex flex-col items-center justify-center relative">
 
         {/* Center content */}
         <div className="text-center">
-          <BowlIcon />
+          <img src={BowlIcon} alt="DailyDish Logo" className="inline-block" />
 
-          <h1 className="mt-4 text-3xl font-semibold text-lightOlive">
+          <h1 className="mt-4 text-3xl font-semibold text-[#95B974]">
             DailyDish
           </h1>
 
-          <p className="mt-1 text-sm text-[#E6E6D8] opacity-80">
+          <p className="mt-1 text-sm text-[#CEDEBD] opacity-80">
             Everyday ingredients. Everyday magic.
           </p>
         </div>
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,27 @@
-
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from '../features/landingpage/pages/LandingPage';
 import Login from '../features/auth/pages/Login';
+import SplashScreen from '../features/splashscreen/SplashScreen';
 
+const SplashToLanding = () => {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return showSplash ? <SplashScreen /> : <LandingPage />;
+};
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/" element={<SplashToLanding />} />
       <Route path="login" element={<Login />} />
    
       </Routes>
